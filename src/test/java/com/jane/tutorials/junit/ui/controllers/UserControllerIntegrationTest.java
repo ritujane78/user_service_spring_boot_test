ed34ed0f551ccf444534c651a1fc7fb3fa94009a
Ritu Bafna <ritujane78@gmail.com>
2025-07-18 13:23:41 +0545

    add precendences for ports

--- src/test/java/com/jane/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
+++ src/test/java/com/jane/tutorials/junit/ui/controllers/UserControllerIntegrationTest.java
@@ -3,17 +3,26 @@ package com.jane.tutorials.junit.ui.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalManagementPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations="/application-test.properties",
-        properties = "server.port=8888")
+        properties = "server.port=0")
 public class UserControllerIntegrationTest {
     @Value("${server.port}")
-    String serverPort;
+    int serverPort;
+
+    @LocalServerPort
+    int localServerPort;
+
+
     @Test
     void contextLoads() {
 //        precedence : properties defined in the java code > application-test.properties > application.properties
         System.out.println("server port = "+ serverPort);
+
+        System.out.println("Local server port = " + localServerPort);
     }
 }
